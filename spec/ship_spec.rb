b81ea0f88240d301df6bfcdc0a0faeb01795ec0a
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-07-13 00:05:50 -0600

    complete tests for Ship class. All passing

---
+++ spec/ship_spec.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+require './lib/ship'
+
+RSpec.describe Ship do
+
+  context 'initialize' do
+    cruiser = Ship.new("Cruiser", 3)
+
+    it 'exists with attributes' do
+      expect(cruiser).to be_a(Ship)
+      expect(cruiser.name).to eq("Cruiser")
+      expect(cruiser.length).to eq(3)
+      expect(cruiser.health).to eq(3)
+    end
+
+    it 'is still afloat' do
+      expect(cruiser.sunk?).to be(false)
+    end
+
+    it 'takes damage and sinks' do
+      cruiser.hit
+      expect(cruiser.health).to eq(2)
+      expect(cruiser.sunk?).to be(false)
+      cruiser.hit
+      cruiser.hit
+      expect(cruiser.sunk?).to be(true)
+    end
+  end
+
+end
