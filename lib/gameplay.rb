9a8fe21be0dd996ca8537e0a3b79efb95be80a69
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-07-19 15:10:33 -0600

    created logic for game menu/ ship placement

--- lib/gameplay.rb
+++ lib/gameplay.rb
@@ -3,16 +3,18 @@ require './lib/cell'
 require './lib/board'
 
 class Gameplay
+  attr_reader :ship_array, :board
 
   def initialize
     @board     = Board.new
     @cruiser   = Ship.new('Cruiser', 3)
     @submarine = Ship.new('Submarine', 2)
+    @ship_array = []
   end
 
   def welcome_message
-    "Welcome to BATTLESHIP
-  Enter p to play. Enter q to quit."
+    puts "Welcome to BATTLESHIP
+Enter p to play. Enter q to quit."
 
     if gets.chomp == 'p'
       start_game
@@ -21,12 +23,39 @@ class Gameplay
     end
   end
 
-
   def start_game
    puts "I have laid out my ships on the grid.
 You now need to lay out your two ships.
 The Cruiser is three units long and the Submarine is two units long."
    puts @board.render
+   puts "Enter the first coordinate for the Cruiser (Letter/Number):"
+   @ship_array << gets.chomp
+   puts "Enter the second coordinate for the Cruiser:"
+   @ship_array << gets.chomp
+   puts "Enter the final coordinate for the Cruiser:"
+   @ship_array << gets.chomp
+   place_cruiser
+   puts "Enter the first coordinate for the Submarine (Letter/Number):"
+   @ship_array << gets.chomp
+   puts "Enter the final coordinate for the Submarine:"
+   @ship_array << gets.chomp
+   place_submarine
+  end
+
+  def place_cruiser
+    if @board.valid_placement?(@cruiser, @ship_array)
+      @board.place(@cruiser, @ship_array)
+    else
+      puts "Those are invalid Cruiser coordinates. Please try again:"
+    end
+  end
+
+  def place_submarine
+    if @board.valid_placement?(@cruiser, @ship_array)
+      @board.place(@submarine, @ship_array[3, 4])
+    else
+      puts "Those are invalid Submarine coordinates. Please try again:"
+    end
   end
 
   def end_game
